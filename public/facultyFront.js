f5b8ab918c5675447af8343b229921b912727c31
Lakshmikanth2001 <lakshmikanth2001@gmail.com>
2020-10-03 16:32:07 +0530

    added student interface and data.json which contains data about branches and subjects

---
+++ public/facultyFront.js
@@ -0,0 +1,8 @@
+function change_branch(){
+    const branch=document.getElementById("selectyear").value;
+    console.log(branch);
+    console.log('branches need to be changed');
+}
+function change_subject(){
+    console.log("subject needed to be changed");
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const mongoose = require('mongoose');
 require('dotenv').config({path: __dirname + '/.env'});
+const fs = require('fs');
 
+
+//---------------Make Upload Directory8--------------------//
+function MakdeUploadDir(path,data){
+    if(data==null){
+      return null;
+    }
+    else{
+      let keys=Object.keys(data);
+      keys.forEach(element => {
+        if(!fs.existsSync(path+"/"+element)){
+          fs.mkdirSync(path+"/"+element);
+        }
+        MakdeUploadDir(path+element+'/',data[element]);
+      });
+    }
+}
+
+
+let rawdata = fs.readFileSync('src\\data.json');
+let data=JSON.parse(rawdata);
+
+path=__dirname+"/uploads/"
+MakdeUploadDir(path,data.years);// this will create a os directory as data.json data
+
+//---------------Make Upload Directory8--------------------//
 
 
 //----------------DB Section-------------------------------//
@@ -17,7 +43,11 @@ db.once('open', function() {
 });
 
 const UserSchema=mongoose.Schema({
+  Name:String,
+  Designation:String,
+  Branch:String,
   Email:String,
+  Year:String,
   Password:String
 });
 
@@ -51,15 +81,93 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:position", function (req, res) {
-  //res.sendFile(__dirname + "/Login.html");
-  const data={
-    Warning:""
+  if(req.params.position === "SignUp"){
+    res.render("SignUp");
   }
-  res.render("Login",data);
+  else{
+    const data={
+      Warning:""
+    }
+    res.render("Login",data);
+
+  }
+  //res.sendFile(__dirname + "/Login.html");
+  
 });
 
 //---------------------Gets-----------------------//
 
+
+async function StudentLogin(req,res){
+  await StudentDB.findOne(req.body,function(err,responce){
+    if(err){
+      res.send(err);
+    }
+    else if(responce==null){
+        // should raise an error
+        //res.sendFile(__dirname+'/Login.html')
+        const data={
+          Warning:"UserName or Passowrd is wronge",
+        }
+        res.render("Login",data);
+    }
+    else{
+      res.render('student');
+    }
+  });
+}
+
+async function FacultyLogin(req,res){
+  await FacultyDB.findOne(req.body,function(err,responce){
+    if(err){
+      res.send(err);
+    }
+    else if(responce==null){
+      // should raise an error
+       // res.sendFile(__dirname+'/Login.html');
+       const data={
+        Warning:"UserName or Passowrd is wronge",
+      }
+      res.render("Login",data);
+    }
+    else{
+      //res.sendFile(__dirname + "/faculty.html");
+      res.render('faculty');
+    }
+  });
+}
+
+async function UploadRequest(req,res){
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  console.log(req.body);
+  let sampleFile = req.files.UploadedFile;
+  let year=req.body.Year+"\\";
+  let branch=req.body.Branch+"\\";
+  let subject=req.body.subject+"\\";
+  const uploadPath = __dirname + "\\uploads\\" +year+branch+subject+sampleFile.name;
+  console.log(uploadPath);
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
+  });
+}
+
+async function AddUser(req,res){
+  if(req.body.Designation==='student'){
+    const data=new StudentDB(req.body);
+    await data.save();
+    res.send('data is uploaded to data base');
+  }
+  else{
+    const data=new FacultyDB(req.body);
+    await data.save();
+    res.send('data is uploaded to data base');
+  }
+  
+}
 
 
 
@@ -69,55 +177,17 @@ app.post("/:position", async function (req, res) {
   //req.body is Email and Passowrd
   //console.log(req.params);
   if(req.params.position==='Student'){
-    StudentDB.findOne(req.body,function(err,responce){
-      if(err){
-        res.send(err);
-      }
-      else if(responce==null){
-          // should raise an error
-          //res.sendFile(__dirname+'/Login.html')
-          const data={
-            Warning:"UserName or Passowrd is wronge",
-          }
-          res.render("Login",data);
-      }
-      else{
-        res.sendFile("student login is succesfull");
-      }
-    });
+    await StudentLogin(req,res);
   }
-  if (req.params.position === "Faculty") {
-    await FacultyDB.findOne(req.body,function(err,responce){
-      if(err){
-        res.send(err);
-      }
-      else if(responce==null){
-        // should raise an error
-         // res.sendFile(__dirname+'/Login.html');
-         const data={
-          Warning:"UserName or Passowrd is wronge",
-        }
-        res.render("Login",data);
-      }
-      else{
-        //res.sendFile(__dirname + "/faculty.html");
-        res.render('faculty');
-      }
-    })
-    
+  else if (req.params.position === "Faculty") {
+    await FacultyLogin(req,res);
   } 
   else if (req.params.position === "facultyUpload") {
     // we need to access the files here to store them in google drive
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send("No files were uploaded.");
-    }
-    let sampleFile = req.files.UploadedFile;
-    const uploadPath = __dirname + "/uploads/" + sampleFile.name;
-    sampleFile.mv(uploadPath, function (err) {
-      if (err) return res.status(500).send(err);
-
-      res.send("File uploaded!");
-    });
+    await UploadRequest(req,res);
+  }
+  else if(req.params.position === "SignUp"){
+    await AddUser(req,res);
   }
 });
 
